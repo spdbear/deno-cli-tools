0620bd534b371d3ff8c84b9dccc985ba93eca9ce
spdbear <16793732+spdbear@users.noreply.github.com>
2021-03-12 00:14:31 +0900

    確定マスを濡れるようにした

--- picross-solver/index.ts
+++ picross-solver/index.ts
@@ -15,6 +15,8 @@ const loadData = async () => {
   return { w, h, field }
 }
 
+type Field = -1 | 0 | 1
+
 const showArray = (field: number[][]) => {
   const dataMap = new Map()
   dataMap.set(0, '⬜')
@@ -25,11 +27,43 @@ const showArray = (field: number[][]) => {
       .map((v) => {
         return dataMap.get(v)
       })
-      .join(' ')
+      .join('')
     console.log(rowStr)
   })
 }
 
-const data = await loadData()
-console.log(data.w, data.h)
-showArray(data.field)
+const fillDefinitiveLine = (lineField: Field[], lineInfo: number[]) => {
+  const calcSum = (arr: number[]) => {
+    return arr.reduce((acc: number, cur: number) => acc + cur, 0) + arr.length - 1
+  }
+  let pos = 0
+  if (calcSum(lineInfo) === lineField.length) {
+    lineInfo.forEach((pxNum) => {
+      lineField = lineField.fill(1, pos, pxNum + pos)
+      pos += pxNum + 1
+    })
+  }
+  return lineField
+}
+
+const fillStage = (
+  lineFields: Field[][],
+  rowInfo: number[][],
+  colInfo: number[][]
+) => {
+  const rowFilledFields = lineFields.map((lineField, index) =>
+    fillDefinitiveLine(lineField, rowInfo[index])
+  )
+  const transpose = (arr: Field[][]) =>
+    arr[0].map((_, c) => arr.map((a: Field[]) => a[c]))
+  const transposedFields = transpose(rowFilledFields)
+  return transpose(
+    transposedFields.map((lineField, index) =>
+      fillDefinitiveLine(lineField, colInfo[index])
+    )
+  )
+}
+
+const dt = await loadData()
+console.log(dt.w, dt.h)
+showArray(fillStage(dt.field, dt.w, dt.h))
